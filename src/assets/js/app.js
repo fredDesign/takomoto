f2f217ac1ea1dae7e7cd1629e125bc63ac0692e1
bloxboy <frederic.bloxboy@gmail.com>
2019-06-06 16:15:30 +0200

    add contact form working on Firebase

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,4 +1,41 @@
+//var contactRef =firebase.database().ref().child("contactForm");
+
+var messagesRef = firebase.database().ref().child("contact_takemoto");
+function submitForm(e){
+	e.preventDefault();
+	// Get values
+	var name = getInputVal('name');
+	var company = getInputVal('company');
+	var email = getInputVal('email');
+	var phone = getInputVal('phone');
+	var message = getInputVal('message');
+
+	// Save message
+	saveMessage(name, company, email, phone, message);
+
+
+	// Clear form
+	$('#contactForm')[0].reset();
+}
+
+// Function to get get form values
+function getInputVal(id) {
+	return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name, company, email, phone, message) {
+	var newMessageRef = messagesRef.push();
+	newMessageRef.set({
+		name: name,
+		company: company,
+		email: email,
+		phone: phone,
+		message: message
+	});
+}
 $(document).ready(function () {
+	$('#contactForm').on('submit',submitForm);
 	//initialize swiper when document ready
 	var mySwiper = new Swiper ('.swiper-container', {
 		// Optional parameters
